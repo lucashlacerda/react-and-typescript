864fe8f6b156339bce43c57b3d31229e5a7fc029
Lucas Lacerda <contatollacerda@gmail.com>
2023-01-20 15:32:29 -0300

    Basic props.

---
+++ react-typescript-demo/src/components/PersonList.tsx
@@ -0,0 +1,20 @@
+type PersonListProps = {
+  names: {
+    first: string;
+    last: string;
+  }[];
+};
+
+function PersonList(props: PersonListProps) {
+  return (
+    <div>
+      {props.names.map((name) => (
+        <h2 key={name.first}>
+          {name.first} {name.last}
+        </h2>
+      ))}
+    </div>
+  );
+}
+
+export default PersonList;
